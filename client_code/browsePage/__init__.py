f4d5f12507167acd585e50eb1c5874dfbee0417e
espiritu.mickaeladanna@gmail.com <espiritu.mickaeladanna@gmail.com>
2024-07-03 20:07:51 +0000

    Edited browsePage.RowTemplate4 and browsePage; Deleted ServerModule1

--- client_code/browsePage/__init__.py
+++ client_code/browsePage/__init__.py
@@ -10,7 +10,20 @@ class browsePage(browsePageTemplate):
     def __init__(self, **properties):
       # Set Form properties and Data Bindings.
       self.init_components(**properties)
-      self.secDataPanel.items = anvil.server.call('get_book_list')
+
+      self.populate_component(self.secDataPanel, 'get_book_list')
+
+    def populate_component(self, component, data_fetch_method):
+        try:
+            data_list = anvil.server.call(data_fetch_method)
+        
+            # Assuming that the component's items can be set
+            if isinstance(data_list, list):
+                component.items = data_list
+            else:
+                component.items = []  # Clear the data if data_list is not a list
+        except Exception as e:
+            print("Error occurred while populating component:", e)
 
     def cmdSearchBtn_click(self, **event_args):
       strSearchItem = self.txtSearchBox.text
